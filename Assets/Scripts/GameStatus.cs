dac017b8c400579a6db503feddde2c82b8787d8f
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2019-01-27 13:30:47 +0100

    Display player score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour {
+    [SerializeField] TextMeshProUGUI scoreText;
+
     // Config params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 10;
@@ -10,13 +13,24 @@ public class GameStatus : MonoBehaviour {
     // States
     [SerializeField] int currentScore = 0;
 
-	// Update is called once per frame
-	void Update () {
+    private void UpdateScoreText()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    private void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update () {
         Time.timeScale = gameSpeed;
 	}
 
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        UpdateScoreText();
     }
 }
